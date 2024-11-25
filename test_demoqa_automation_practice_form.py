b9ee2d1204c6223d8c54ec1401c1a6c4331a85f7
pavelkalinchuk <715m4s@gmail.com>
2024-11-25 23:20:26 +0300

    Сделан тест для поля email. Создан файл .gitignore.

--- test_demoqa_automation_practice_form.py
+++ test_demoqa_automation_practice_form.py
@@ -1,8 +1,13 @@
 from selene import browser, be, have
 
 
-def test_positive_text():
+def test_name():
     browser.element('[id="firstName"]').click().type("Pavel")
     browser.element('[id="lastName"]').click().type("Kalinchuk")
     assert browser.element('[id="firstName"]').should(be.visible).should(have.value('Pavel'))
     assert browser.element('[id="lastName"]').should(be.visible).should(have.value('Kalinchuk'))
+
+
+def test_email():
+    browser.element('[id="userEmail"]').click().type("pavelkalinchuk@mail.tst")
+    assert browser.element('[id="userEmail"]').should(be.visible).should(have.value('pavelkalinchuk@mail.tst'))
