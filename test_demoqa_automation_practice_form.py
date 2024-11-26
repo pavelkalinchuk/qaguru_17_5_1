f02ddf3a036f9b2737597c2a8588677cef7e1a9d
pavelkalinchuk <715m4s@gmail.com>
2024-11-27 00:49:15 +0300

    Сделаны тесты для полей: Пол, Телефон, Дата рождения.

--- test_demoqa_automation_practice_form.py
+++ test_demoqa_automation_practice_form.py
@@ -1,3 +1,5 @@
+import time
+
 from selene import browser, be, have
 
 
@@ -11,3 +13,48 @@ def test_name():
 def test_email():
     browser.element('[id="userEmail"]').click().type("pavelkalinchuk@mail.tst")
     assert browser.element('[id="userEmail"]').should(be.visible).should(have.value('pavelkalinchuk@mail.tst'))
+
+
+def test_gender():
+    browser.element('label[for="gender-radio-1"]').click()
+    assert browser.element('#gender-radio-1').should(be.selected)
+
+
+def test_mobile():
+    browser.element('#userNumber').click().type("99-23-67-0")
+    assert browser.element('#userNumber').should(be.visible).should(have.value('99-23-67-0'))
+
+
+def test_date_of_birth():
+    browser.element('#dateOfBirthInput').click()
+    browser.element('.react-datepicker-popper').should(be.visible)
+    browser.element('.react-datepicker__month-select').should(be.visible).element('[value="0"]').click()
+    browser.element('.react-datepicker__year-select').should(be.visible).element('[value="2000"]').click()
+    browser.element('.react-datepicker__week').should(be.visible).element('.react-datepicker__day.react-datepicker__day--001').click()
+    assert browser.element('#dateOfBirthInput').should(have.value('01 Jan 2000'))
+
+
+'''
+def test_subject():
+    pass
+
+
+def test_hobbies():
+    pass
+
+
+def test_pictures():
+    pass
+
+
+def test_current_address():
+    pass
+
+
+def test_state_nd_city():
+    pass
+
+
+def test_submit():
+    pass
+'''
